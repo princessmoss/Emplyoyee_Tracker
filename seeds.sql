cf9f63232ce504265355fa7606bee4465cc19438
Princess M <princessmosse@gmail.com>
2021-06-06 20:35:26 -0400

    adding in seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -13,3 +13,26 @@ VALUES ('Toby', 'Flannagan', 5, 2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Robert', 'California', 6, null);
 
+INSERT INTO department (department_name)
+VALUES ('Management');
+INSERT INTO department (department_name)
+VALUES ('Sales');
+INSERT INTO department (department_name)
+VALUES ('Accounting');
+INSERT INTO department (department_name)
+VALUES ('Reception');
+INSERT INTO department (department_name)
+VALUES ('Human Resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('General Manager', 120000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Salesman', 80000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Accountant', 90000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Receptionist', 40000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Human Resource Officer', 75000, 5);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('CEO', 250000, null);
