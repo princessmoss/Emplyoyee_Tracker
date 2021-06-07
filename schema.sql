03ca02bbb151913856cbabc65351a22a2a2af08e
Princess M <princessmosse@gmail.com>
2021-06-06 20:34:51 -0400

    adding files

--- schema.sql
+++ schema.sql
@@ -1,31 +1,27 @@
-DROP DATABASE IF EXISTS employees_db;
+DROP DATABASE IF EXISTS employee_DB;
+CREATE database employee_DB;
 
--- Creates the "employees_db" database --
-CREATE DATABASE employees_db;
+USE employee_DB;
 
--- Makes it so all of the following code will affect employees_db --
-USE employees_db;
-
--- Creates the table "people" within employees_db --
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
+    id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(50) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 0) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
 );
---
+++ seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Michael', 'Sott', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jim', 'Halpert', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Dwight', 'Schrute', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Pam', 'Beasley', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Kevin', 'Malone', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Toby', 'Flannagan', 5, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Robert', 'California', 6, null);
+
